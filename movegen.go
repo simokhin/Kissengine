7d645843bb8009fb675611fc2fe2f63af2c77f19
Nikita Simokhin <just.simokhin@gmail.com>
2026-07-15 22:21:34 +0300

    Add function for generating moves for sliding pieces

--- movegen.go
+++ movegen.go
@@ -1,10 +1,59 @@
 package main
 
+import "slices"
+
 var KingOffsets = [8]Square{-1, +1, -15, +15, -16, +16, -17, +17}
 var KnightOffsets = [8]Square{-14, +14, -18, +18, -31, +31, -33, +33}
+var BishopOffsets = [4]Square{+15, -15, +17, -17}
+var RookOffsets = [4]Square{+1, -1, +16, -16}
+
+func GenerateSlidingPieceMoves(from Square, board BoardState, piece Piece) []Move {
+	var offsets []Square
+
+	switch piece {
+	case Bishop:
+		offsets = BishopOffsets[:]
+	case Rook:
+		offsets = RookOffsets[:]
+	case Queen:
+		offsets = slices.Concat(BishopOffsets[:], RookOffsets[:])
+	}
+
+	moves := []Move{}
+
+	pieceColor := board.squares[from].Color()
+	for i := range offsets {
+		candidateSquare := from
+		for {
+			candidateSquare += offsets[i]
+
+			if candidateSquare&0x88 == 0 {
+				targetPiece := board.squares[candidateSquare]
+
+				if targetPiece == Empty {
+					newMove := NewMove(from, candidateSquare, 0, QuietMove, 0)
+					moves = append(moves, newMove)
+				} else {
+					if targetPiece.Color() != pieceColor {
+						newMove := NewMove(from, candidateSquare, 0, Capture, targetPiece)
+						moves = append(moves, newMove)
+						break
+					} else {
+						break
+					}
+				}
+			} else {
+				break
+			}
+		}
+	}
+
+	return moves
+}
 
 func GenerateJumpingPieceMoves(from Square, board BoardState, piece Piece) []Move {
 	var offsets []Square
+
 	switch piece {
 	case King:
 		offsets = KingOffsets[:]
@@ -43,4 +92,3 @@ func GenerateJumpingPieceMoves(from Square, board BoardState, piece Piece) []Mov
 	return moves
 
 }
-
