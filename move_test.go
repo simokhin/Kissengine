3b0cadc7a27856b5756b9ef61fd102fc51d3004b
Nikita Simokhin <just.simokhin@gmail.com>
2026-07-15 16:40:04 +0300

    Add test for Move

--- move_test.go
+++ move_test.go
@@ -3,5 +3,33 @@ package main
 import "testing"
 
 func TestMoveRoundTrip(t *testing.T) {
+	from := FileRankToSquareIndex(SquareNotationToFileRank("e7"))
+	to := FileRankToSquareIndex(SquareNotationToFileRank("e8"))
 
+	move := NewMove(from, to, Queen, Capture, Rook|Black)
+
+	gotTo := move.To()
+	if gotTo != to {
+		t.Fatalf("want %d, got %d", to, gotTo)
+	}
+
+	gotFrom := move.From()
+	if gotFrom != from {
+		t.Fatalf("want %d, got %d", from, gotFrom)
+	}
+
+	gotFlag := move.Flag()
+	if gotFlag != Capture {
+		t.Fatalf("want %d, got %d", Capture, gotFlag)
+	}
+
+	gotPromotion := move.Promotion()
+	if gotPromotion != Queen {
+		t.Fatalf("want %d, got %d", Queen, gotPromotion)
+	}
+
+	gotCapturedPiece := move.CapturedPiece()
+	if gotCapturedPiece != Rook|Black {
+		t.Fatalf("want %d, got %d", Rook|Black, gotCapturedPiece)
+	}
 }
