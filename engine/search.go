e83af6a911bb2560967304d93c5850813d70b03d
Nikita Simokhin <just.simokhin@gmail.com>
2026-07-17 02:46:29 +0300

    Implement moves sorting

--- engine/search.go
+++ engine/search.go
@@ -1,11 +1,29 @@
 package engine
 
-import "time"
+import (
+	"sort"
+	"time"
+)
 
 const (
 	Infinity Evaluation = 1_000_000
 	Mate     Evaluation = 100_000
 )
+
+func moveScore(board BoardState, move Move) int {
+	if move.CapturedPiece() == Empty {
+		return 0
+	}
+	attacker := board.PieceAt(move.From())
+	return int(pieceValues[move.CapturedPiece().Type()])*10 - int(pieceValues[attacker.Type()])
+}
+
+func orderMoves(board BoardState, moves []Move) []Move {
+	sort.Slice(moves, func(i, j int) bool {
+		return moveScore(board, moves[i]) > moveScore(board, moves[j])
+	})
+	return moves
+}
 
 func FindBestMoveByTime(board BoardState, timeLimit time.Duration) Move {
 	deadline := time.Now().Add(timeLimit)
@@ -25,6 +43,7 @@ func FindBestMove(board BoardState, depth int) Move {
 	var bestMove Move
 
 	moves := GenerateLegalMoves(board)
+	orderMoves(board, moves)
 
 	best := -Infinity
 
@@ -46,6 +65,8 @@ func NegaMax(board BoardState, depth int, alpha, beta Evaluation) Evaluation {
 	}
 
 	moves := GenerateLegalMoves(board)
+	orderMoves(board, moves)
+
 	if len(moves) == 0 {
 		if board.InCheck() {
 			return -(Mate + Evaluation(depth))
--- gui/main.go
+++ gui/main.go
@@ -11,9 +11,9 @@ func main() {
 	ebiten.SetWindowSize(800, 800)
 	ebiten.SetWindowTitle("Kissengine GUI")
 
-	testFEN := "k7/7P/8/3q4/8/5n2/8/7K w - - 0 1"
+	// testFEN := "k7/7P/8/3q4/8/5n2/8/7K w - - 0 1"
 
-	game := &Game{board: engine.ParseFEN(testFEN), humanColor: engine.White}
+	game := &Game{board: engine.ParseFEN(engine.StartFen), humanColor: engine.White}
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
