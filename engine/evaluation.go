4372f0f4bf2ea848304c41b1dcea57e89e670e8e
Nikita Simokhin <just.simokhin@gmail.com>
2026-07-17 20:09:35 +0300

    Evaluate passed pawns and rook on open files

--- engine/evaluation.go
+++ engine/evaluation.go
@@ -1,5 +1,7 @@
 package engine
 
+import "math/bits"
+
 var pieceValues = [7]Evaluation{
 	Pawn:   100,
 	Knight: 300,
@@ -13,30 +15,45 @@ const (
 	bishopPairBonus     Evaluation = 30
 	doubledPawnPenalty  Evaluation = 15
 	isolatedPawnPenalty Evaluation = 15
+	openFileBonus       Evaluation = 20
+	semiOpenFileBonus   Evaluation = 10
 )
 
+var passedPawnBonus = [8]Evaluation{0, 5, 10, 20, 35, 60, 100, 0}
+
 type Evaluation int
+
+// aheadMask returns a bitmask of the ranks strictly ahead of rank, from color's point of view
+// (higher ranks for White, lower ranks for Black).
+func aheadMask(rank int, color Piece) uint8 {
+	if color == White {
+		return uint8(0xFF) << (rank + 1)
+	}
+	return uint8(0xFF) >> (8 - rank)
+}
 
 func Evaluate(board BoardState) Evaluation {
 	var evaluation Evaluation
 	phase := gamePhase(board)
 	var whiteBishops, blackBishops int
-	var whitePawnsByFile [8]int
-	var blackPawnsByFile [8]int
+	var whitePawnRanks, blackPawnRanks [8]uint8
+	var pawnSquares []Square
+	var rookSquares []Square
 
 	for i := range board.squares {
-		file, _ := SquareIndexToFileRank(Square(i))
 		piece := board.squares[i]
 		if piece == Empty {
 			continue
 		}
 
 		if piece.Type() == Pawn {
+			file, rank := SquareIndexToFileRank(Square(i))
 			if piece.Color() == White {
-				whitePawnsByFile[file] += 1
+				whitePawnRanks[file] |= 1 << rank
 			} else {
-				blackPawnsByFile[file] += 1
+				blackPawnRanks[file] |= 1 << rank
 			}
+			pawnSquares = append(pawnSquares, Square(i))
 		}
 
 		if piece.Type() == Bishop {
@@ -45,6 +62,10 @@ func Evaluate(board BoardState) Evaluation {
 			} else {
 				blackBishops++
 			}
+		}
+
+		if piece.Type() == Rook {
+			rookSquares = append(rookSquares, Square(i))
 		}
 
 		var positional Evaluation
@@ -63,41 +84,79 @@ func Evaluate(board BoardState) Evaluation {
 		}
 	}
 
-	for i := range board.squares {
-		piece := board.squares[i]
-		if piece.Type() != Pawn {
-			continue
+	for _, square := range pawnSquares {
+		piece := board.squares[square]
+		file, rank := SquareIndexToFileRank(square)
+
+		ownRanks, opponentRanks := &whitePawnRanks, &blackPawnRanks
+		if piece.Color() == Black {
+			ownRanks, opponentRanks = &blackPawnRanks, &whitePawnRanks
 		}
 
-		file, _ := SquareIndexToFileRank(Square(i))
+		var bonus Evaluation
 
-		var pawnsByFile [8]int
-		if piece.Color() == White {
-			pawnsByFile = whitePawnsByFile
-		} else {
-			pawnsByFile = blackPawnsByFile
-		}
-
-		var penalty Evaluation
-		if pawnsByFile[file] > 1 {
-			penalty += doubledPawnPenalty
+		if bits.OnesCount8(ownRanks[file]) > 1 {
+			bonus -= doubledPawnPenalty
 		}
 
 		isolated := true
-		if file > 0 && pawnsByFile[file-1] > 0 {
+		if file > 0 && ownRanks[file-1] != 0 {
 			isolated = false
 		}
-		if file < 7 && pawnsByFile[file+1] > 0 {
+		if file < 7 && ownRanks[file+1] != 0 {
 			isolated = false
 		}
 		if isolated {
-			penalty += isolatedPawnPenalty
+			bonus -= isolatedPawnPenalty
+		}
+
+		mask := aheadMask(rank, piece.Color())
+		passed := true
+		for _, f := range [3]int{file - 1, file, file + 1} {
+			if f < 0 || f > 7 {
+				continue
+			}
+			if opponentRanks[f]&mask != 0 {
+				passed = false
+				break
+			}
+		}
+		if passed {
+			promotionRank := rank
+			if piece.Color() == Black {
+				promotionRank = 7 - rank
+			}
+			bonus += passedPawnBonus[promotionRank]
 		}
 
 		if piece.Color() == board.SideToMove().Color() {
-			evaluation -= penalty
+			evaluation += bonus
 		} else {
-			evaluation += penalty
+			evaluation -= bonus
+		}
+	}
+
+	for _, square := range rookSquares {
+		piece := board.squares[square]
+		file, _ := SquareIndexToFileRank(square)
+
+		var bonus Evaluation
+		if whitePawnRanks[file] == 0 && blackPawnRanks[file] == 0 {
+			bonus = openFileBonus
+		} else {
+			ownRanks := whitePawnRanks
+			if piece.Color() == Black {
+				ownRanks = blackPawnRanks
+			}
+			if ownRanks[file] == 0 {
+				bonus = semiOpenFileBonus
+			}
+		}
+
+		if piece.Color() == board.SideToMove().Color() {
+			evaluation += bonus
+		} else {
+			evaluation -= bonus
 		}
 	}
 
